0a3ac5cc7a5211b9d81e5afdf967ac5c0cfa1bd7
Thorgils Hjalmarsson <thorgils@hi.is>
2025-09-12 10:27:18 +0000

    Init

---
+++ configuration.nix
@@ -0,0 +1,121 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, lib, fetchTarball, ... }: {
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+  # My services :)
+  services.spice-vdagentd.enable = true;
+  services.xserver = {
+    enable = true;
+    displayManager.sddm.enable = true;
+    desktopManager.plasma5.enable = true;
+  };
+  # My programs
+  programs.hyprland.enable = true;
+
+# Bootloader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.useOSProber = true;
+
+# Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  networking.hostName = "nixos"; # Define your hostname.
+# networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+# Configure network proxy if necessary
+# networking.proxy.default = "http://user:password@proxy:port/";
+# networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+# Enable networking
+	  networking.networkmanager.enable = true;
+
+# Set your time zone.
+  time.timeZone = "Atlantic/Reykjavik";
+
+# Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+	  LC_ADDRESS = "is_IS.UTF-8";
+	  LC_IDENTIFICATION = "is_IS.UTF-8";
+	  LC_MEASUREMENT = "is_IS.UTF-8";
+	  LC_MONETARY = "is_IS.UTF-8";
+	  LC_NAME = "is_IS.UTF-8";
+	  LC_NUMERIC = "is_IS.UTF-8";
+	  LC_PAPER = "is_IS.UTF-8";
+	  LC_TELEPHONE = "is_IS.UTF-8";
+	  LC_TIME = "is_IS.UTF-8";
+  };
+
+# Configure keymap in X11
+  services.xserver.xkb = {
+	  layout = "us";
+	  variant = "dvorak";
+  };
+
+# Configure console keymap
+  console.keyMap = "dvorak";
+
+# Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.gilli = {
+	  isNormalUser = true;
+	  description = "gilli";
+	  extraGroups = [ "networkmanager" "wheel" ];
+	  packages = with pkgs; [];
+  };
+# Experimental features
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+# Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+# spice service
+
+# List packages installed in system profile. To search, run:
+# $ nix search wget
+  environment.systemPackages = with pkgs; [
+#  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+	  wget
+		  git
+		  qutebrowser
+		  vim
+		  hyprland
+		  hyprpaper
+		  kdePackages.sddm
+		  kitty
+  ];
+
+
+# Some programs need SUID wrappers, can be configured further or are
+# started in user sessions.
+# programs.mtr.enable = true;
+# programs.gnupg.agent = {
+#   enable = true;
+#   enableSSHSupport = true;
+# };
+
+# List services that you want to enable:
+
+# Enable the OpenSSH daemon.
+# services.openssh.enable = true;
+
+# Open ports in the firewall.
+# networking.firewall.allowedTCPPorts = [ ... ];
+# networking.firewall.allowedUDPPorts = [ ... ];
+# Or disable the firewall altogether.
+# networking.firewall.enable = false;
+
+# This value determines the NixOS release from which the default
+# settings for stateful data, like file locations and database versions
+# on your system were taken. It‘s perfectly fine and recommended to leave
+# this value at the release version of the first install of this system.
+# Before changing this value read the documentation for this option
+# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.05"; # Did you read the comment?
+
+}
