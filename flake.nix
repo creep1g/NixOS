353bb9a6fdb3123e0fc18d22b8e8f7cfbf9b003a
Toggi <thorgils@hi.is>
2025-09-14 20:43:01 +0000

    Fixed broken flake

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
 	description = "NixOS + Hyprland + Dotfiles setup";
 	
 	inputs = {
-		nixpkgs.url = "github:NixOS/nixpkgs/nixos/-unstable";
+		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 		home-manager.url = "github:nix-community/home-manager";
 		home-manager.inputs.nixpkgs.follows = "nixpkgs";
 		dotfiles.url = "github:creep1g/NixOS-Dotfiles"; # Repo
