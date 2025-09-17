db78ba1f3b7ab2b2bfc802bfbe14034d8dabc07e
Toggi <thorgils@hi.is>
2025-09-17 13:28:13 +0000

    Fonts

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -1,19 +1,15 @@
 # modules/fonts.nix
+# /home/gilli/.local/share/OhSoManyFonts/usr/share/fonts
 { config, pkgs, ... }:
 
 let
   # Fetch your GitHub repo with fonts
-  fontRepo = pkgs.fetchFromGitHub {
-    owner = "creep1g";       # replace with your GitHub username
-    repo = "OhSoManyFonts";           # replace with your repo name
-    rev = "803e0bf21d421b5d5f372493585a212c0c227bc4";             # or pin to a commit hash
-    sha256 = "0000000000000000000000000000000000000000000000000000"; # replace with correct hash
-  };
+  fontRepo = /home/gilli/.local/share/OhSoManyFonts/usr/share/fonts
 
   # Package that installs fonts to Nix store
   myFonts = pkgs.runCommand "my-fonts" {} ''
     mkdir -p $out/share/fonts/truetype/myfonts
-    cp ${fontRepo}/usr/share/fonts/** $out/share/fonts/
+    cp ${fontRepo}/* $out/share/fonts/
   '';
 in
 {
