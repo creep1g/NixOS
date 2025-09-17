11f930b90d2f01440a500ab746b6c54fa5044009
Toggi <thorgils@hi.is>
2025-09-17 10:55:06 +0000

    Fonts

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -7,13 +7,13 @@ let
     owner = "creep1g";       # replace with your GitHub username
     repo = "OhSoManyFonts";           # replace with your repo name
     rev = "main";             # or pin to a commit hash
-    sha256 = "803e0bf21d421b5d5f372493585a212c0c227bc4"; # replace with correct hash
+    sha256 = "0000000000000000000000000000000000000000000000000000"; # replace with correct hash
   };
 
   # Package that installs fonts to Nix store
   myFonts = pkgs.runCommand "my-fonts" {} ''
     mkdir -p $out/share/fonts/truetype/myfonts
-    cp ${fontRepo}/*.ttf $out/share/fonts/truetype/myfonts/
+    cp ${fontRepo}/usr/share/fonts/** $out/share/fonts/
   '';
 in
 {
