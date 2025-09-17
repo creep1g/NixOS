6466843f1af41177db6b547c5db78137bbe13e2c
Toggi <thorgils@hi.is>
2025-09-17 15:13:24 +0000

    New commit

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -2,14 +2,23 @@
 # /home/gilli/.local/share/OhSoManyFonts/usr/share/fonts
 { config, pkgs, ... }:
 
-{
-  # Add fonts system-wide
-  #.systemPackages = with pkgs; [
-  #  jetbrains-mono
-#		noto-fonts-emoji
-#		inter
-#  ];
+let
+  # Fetch your GitHub repo with fonts
+  fontRepo = pkgs.fetchFromGitHub {
+    owner = "creep1g";       # replace with your GitHub username
+    repo = "OhSoManyFonts";           # replace with your repo name
+    rev = "main";             # or pin to a commit hash
+    sha256 = "0000000000000000000000000000000000000000000000000000"; # replace with correct hash
+  };
 
+  # Package that installs fonts to Nix store
+  myFonts = pkgs.runCommand "my-fonts" {} ''
+    mkdir -p $out/share/fonts/truetype/myfonts
+    cp ${fontRepo}/monolisa-font/fonts/web/*.woff2 $out/share/fonts/
+  '';
+in
+
+{
   fonts.packages = with pkgs; [
     nerd-fonts.droid-sans-mono
     nerd-fonts.jetbrains-mono
@@ -18,5 +27,6 @@
     inter
     nerd-fonts.dejavu-sans-mono
     nerd-fonts.liberation
+    myFonts
   ];
 }
