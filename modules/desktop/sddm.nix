2bd0cfaae5594d3abdd4d5397e9f8b13809af218
Toggi <thorgils@hi.is>
2025-09-20 11:32:29 +0000

    New commit

--- modules/desktop/sddm.nix
+++ modules/desktop/sddm.nix
@@ -2,7 +2,7 @@
 {
 
 	services.displayManager.sddm.enable = true;
-	services.displayManager.sddm.theme = "elegant-sddm-unstable";
+	services.displayManager.sddm.theme = "elegant";
 
 	environment.systemPackages = with pkgs; [
 		kdePackages.sddm
