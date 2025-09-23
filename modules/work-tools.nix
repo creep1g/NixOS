3395348f09daaa8f731a841fdb8cbad0d12f828b
Toggi <thorgils@hi.is>
2025-09-23 19:35:10 +0000

    New commit

--- modules/work-tools.nix
+++ modules/work-tools.nix
@@ -3,6 +3,8 @@
     environment.systemPackages = with pkgs; [
         jxplorer
         (writeShellScriptBin "jxplorer-wrapped" ''
+            export DISPLAY="$DISPLAY"
+            export XDG_RUNTIME_DIR="$XDG_RUNTIME_DIR"
     
             exec "${pkgs.jxplorer}/opt/jxplorer/jxplorer.sh" "-Djxplorer.config=$HOME/.local/jxplorer" "$@"
         '')
