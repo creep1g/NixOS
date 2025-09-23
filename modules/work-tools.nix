72ac5cd10b4fd3f336d1f8670fcd785a5e9ce1b2
Toggi <thorgils@hi.is>
2025-09-23 19:23:25 +0000

    New commit

--- modules/work-tools.nix
+++ modules/work-tools.nix
@@ -3,7 +3,7 @@
     environment.systemPackages = with pkgs; [
         jxplorer
         (writeShellScriptBin "jxplorer-wrapped" ''
-            exec ${jxplorer}/bin/jxplorer -Djxplorer.config=/path/to/desired/dir "$@"
+            exec ${jxplorer}/bin/jxplorer -Djxplorer.config=~/.local/jxplorer "$@"
         '')
     ];
 
