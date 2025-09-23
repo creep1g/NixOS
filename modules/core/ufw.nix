700f07db296b74cd91acdbf6105a072ef515fa07
Toggi <thorgils@hi.is>
2025-09-23 09:39:08 +0000

    New commit

--- modules/core/ufw.nix
+++ modules/core/ufw.nix
@@ -1,16 +1,26 @@
-{config, pkgs, lib, ... }:
+{config, ... }:
 
 {
-	environment.systemPackages = with pkgs; [
-		ufw
-	];
+    networking.hostname = "nixos"
+    networking.wireless.enable = true; 
+    networking.networkmanager.enable = true;
 
-	services.ufw = {
-		enable = true;
-		defaultDenyIncoming = true;
-		defaultAllowOutgoing = true;
-		allowForward = false;
-		logging = "low";
-	};
+    networking.firewall = {
+        enable = true;                # firewall on
+        logRefusedConnections = true; # Logging (low)
+        allowedTCPPorts = [];         # No incoming TCP allowed
+        allowedUDPPorts = [];         # No incoming UDP allowed
+        allowPing = true;             # optional, set to false to block ping
+    };
+
+    # Explicit defaults (already match UFW defaults you want)
+    networking.firewall.checkReversePath = "loose"; # prevent issues with routed traffic
+
+    # Optional: extra rules for dropping invalid packets
+    networking.firewall.extraCommands = ''
+        nft add rule inet filter input ct state invalid drop
+        nft add rule inet filter forward drop
+    '';
+        
 
 }
