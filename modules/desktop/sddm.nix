c001d1217f6d8abd3d942089a6abdd6ab5e05cfe
Toggi <thorgils@hi.is>
2025-09-23 10:10:02 +0000

    New commit

--- modules/desktop/sddm.nix
+++ modules/desktop/sddm.nix
@@ -1,16 +1,24 @@
 { config, pkgs, ... }: 
 {
 
-	environment.systemPackages = with pkgs; [
-		kdePackages.sddm
-		sddm-chili-theme
-		elegant-sddm
-	];
+   environment.systemPackages = with pkgs; [
+       kdePackages.sddm
+    #   sddm-chili-theme
+    #   elegant-sddm
+    ];
 
-	services.displayManager.sddm ={
-		enable = true;
-	    theme = "Elegant";
-	};
+    services.displayManager.sddm ={
+        enable = true;
+        wayland.enable = true;
+        package = pkgs.kdePackages.sddm;
+        extraPackages = with pkgs; [
+            (sddm-astronaut.override { embeddedTheme = "black_hole"; })
+            kdePackages.qtsvg
+            kdePackages.qtmultimedia
+            kdePackages.qtvirtualkeyboard
+        ];
+        theme = "sddm-astronaut-theme";
+    };
 
 
 
