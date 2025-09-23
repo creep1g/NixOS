d6688c6190d6b923afbcc4727f8ab8c5e4919b59
Toggi <thorgils@hi.is>
2025-09-23 12:47:13 +0000

    New commit

--- modules/desktop/sddm.nix
+++ modules/desktop/sddm.nix
@@ -1,34 +1,31 @@
 { config, pkgs, ... }: 
 {
 
-    #environment.systemPackages = with pkgs; [
+    environment.systemPackages = with pkgs; [
     #    kdePackages.sddm
     #    sddm-chili-theme
     #    elegant-sddm
-    #];
+        sddm-astronaut-theme
+            
+    ];
     #services.xserver.desktopManager.plasma5.enable = true;
 
-    #services.displayManager.sddm ={
-    #   enable = true;
-    #    wayland.enable = true;
-    #    package = pkgs.kdePackages.sddm;
-    #    extraPackages = with pkgs; [
-    #        #(sddm-astronaut.override { embeddedTheme = "black_hole"; })
-    #        kdePackages.qtsvg
-    #        kdePackages.qtmultimedia
-    #        kdePackages.qtvirtualkeyboard
-    #    ];
-    #    theme = "sddm-chili-theme";
-    #};
+    services.displayManager.sddm ={
+       enable = true;
+        wayland.enable = true;
+        package = pkgs.kdePackages.sddm;
+        extraPackages = with pkgs; [
+            #(sddm-astronaut.override { embeddedTheme = "black_hole"; })
+            kdePackages.qtsvg
+            kdePackages.qtmultimedia
+            kdePackages.qtvirtualkeyboard
+        ];
+        theme = "sddm-chili-theme";
+    };
 
     # /etc/nixos/configuration.nix
 
     services.xserver.desktopManager.plasma6.enable = true; # Example for Plasma, which is Qt5-based
-    services.displayManager.sddm = {
-        enable = true;
-         package = pkgs.sddm; 
-        theme = "sddm-chili-theme";
-    };
 
     # Add the necessary Qt5 packages for your theme
     environment.systemPackages = with pkgs; [
