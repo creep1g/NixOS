6efcae854b77f83d721d5d9b66690fe5b24f4f19
Thorgils <thorgils@hi.is>
2025-10-01 10:21:55 +0000

    New commit

--- modules/desktop/hyprland/config.nix
+++ modules/desktop/hyprland/config.nix
@@ -62,11 +62,12 @@
         follow_mouse = 1;
         sensitivity = 0;
         touchpad.natural_scroll = true;
-        device:ELECOM TrackBall Mouse HUGE TrackBall {
+
+      };
+      device:ELECOM TrackBall Mouse HUGE TrackBall {
             scroll_method=on_button_down
             scroll_button=BTN_TASK
-        }
-      };
+        };
 
       cursor.no_hardware_cursors = true;
 
